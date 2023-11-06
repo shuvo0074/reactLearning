84c7c037463bfda5cef18c110891e6f0f6352156
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-11-06 11:13:19 +0600

    use memo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMemo, useState } from 'react';
+
+const ExpensiveCalculation = ({ number }) => {
+  const calculateFactorial = (n) => {
+    if (n < 0) {
+      return -1;
+    }
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n * calculateFactorial(n - 1);
+  };
+
+  const factorial = useMemo(() => {
+    console.log("Calculating factorial",number);
+    return calculateFactorial(number);
+  }, [number]);
+
+  return (
+    <p>
+      {number}! = {factorial}
+    </p>
+  );
+};
+
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="number" value={number} onChange={handleChange} />
+        <ExpensiveCalculation number={number} />
       </header>
     </div>
   );
